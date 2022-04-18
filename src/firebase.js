e199baa15f6aca1652f4d1406f965ca8591b303f
AlexAbrello <abrello063@gmai.com>
2022-04-18 13:14:58 +0300

    start

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FAREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FAREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FAREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FAREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FAREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FAREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
